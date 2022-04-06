f4a3c0376e62fc9d511ae5aa1dd7908aed1aa1e4
Vijay Prasath <vijay.prasath@ibm.com>
2022-04-06 12:27:15 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule,Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { UserRegComponent } from './user-reg/user-reg.component';
+import { AppRoutingModule } from './app-routing.module';
 
+const routes:Routes=[
+  {path:'user-reg' , component : UserRegComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,UserRegComponent]
 })
 export class AppModule { }
